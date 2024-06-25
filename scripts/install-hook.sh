c626ce60ba797a49d836b13fb65456ed655dbcb4
Eva <mioyou48@gmail.com>
2024-06-25 16:00:27 +0800

    feat: pre-commit test done.

--- scripts/install-hook.sh
+++ scripts/install-hook.sh
@@ -7,4 +7,4 @@ cp scripts/pre-push.sh $GIT_DIR/hooks/pre-push
 cp scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
 
 chmod +x $GIT_DIR/hooks/pre-push $GIT_DIR/hooks/pre-commit
-echo "Done! "
+echo "Done!"
