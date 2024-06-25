52fd189de8f51639639b948ae9c581072bda939f
Eva <mioyou48@gmail.com>
2024-06-25 13:53:41 +0800

    feat: Initial settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'build',
+  images: {
+    loader: 'akamai',
+    path: '',
+    unoptimized: true,
+  },
+  assetPrefix: './',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,13 @@
+import { type FC } from 'react';
+
+import { Box, Typography } from '@mui/material';
+
+const SideMenu: FC = () => {
+  return (
+    <Box>
+      <Typography>Eva Demo</Typography>
+    </Box>
+  );
+};
+
+export default SideMenu;
