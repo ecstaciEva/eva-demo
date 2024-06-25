4cb486c828a176247b870eba3b75553ce5e3824d
Eva <mioyou48@gmail.com>
2024-06-25 19:43:38 +0800

    feat: autofix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import SideMenu from '@/components/SideMenu';
+
+import { Container } from '@mui/material';
 
 import '@/styles/globals.css';
-import Head from 'next/head';
-import { Container } from '@mui/material';
-import SideMenu from '@/components/SideMenu';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Eva's Blog</title>
+        <title>{`Eva's Blog`}</title>
       </Head>
       <Container>
         <SideMenu />
