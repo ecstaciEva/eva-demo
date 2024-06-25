80ee2a5f956353a6d480904a4b346cecd91cbf9f
Eva <mioyou48@gmail.com>
2024-06-25 15:21:10 +0800

    feat: update install-hook script

--- scripts/install-hook.sh
+++ scripts/install-hook.sh
@@ -5,4 +5,6 @@ GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks..."
 cp scripts/pre-push.sh $GIT_DIR/hooks/pre-push
 cp scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
+
+chmod +x $GIT_DIR/hooks/pre-push $GIT_DIR/hooks/pre-commit
 echo "Done!"
